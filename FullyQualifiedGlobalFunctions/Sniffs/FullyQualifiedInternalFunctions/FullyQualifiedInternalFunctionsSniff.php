11b11d7d2e0eb56a527567e8f7449d0c22e87831
soderlind <per@soderlind.no>
2020-01-07 12:58:45 +0100

    Add option onlyOptimizedFunctions

--- FullyQualifiedGlobalFunctions/Sniffs/FullyQualifiedInternalFunctions/FullyQualifiedInternalFunctionsSniff.php
+++ FullyQualifiedGlobalFunctions/Sniffs/FullyQualifiedInternalFunctions/FullyQualifiedInternalFunctionsSniff.php
@@ -7,12 +7,59 @@ namespace Soderlind\FullyQualifiedGlobalFunctions\Sniffs;
 
 use PHP_CodeSniffer\Sniffs\Sniff;
 use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Config;
+
 
 final class FullyQualifiedGlobalFunctionsSniff implements Sniff {
 
+	private $sniff = 'FullyQualifiedGlobalFunctions';
 	private $fixer;
 	private $stackPtr;
-	private $globalFunctions = [];
+	private $globalFunctions        = [];
+	private $onlyOptimizedFunctions = null;
+	private $optimizedFunctions     = [
+		// @see https://github.com/php/php-src/blob/PHP-7.4/Zend/zend_compile.c "zend_try_compile_special_func"
+		'array_key_exists'     => true,
+		'array_slice'          => true,
+		'assert'               => true,
+		'boolval'              => true,
+		'call_user_func'       => true,
+		'call_user_func_array' => true,
+		'chr'                  => true,
+		'count'                => true,
+		'defined'              => true,
+		'doubleval'            => true,
+		'floatval'             => true,
+		'func_get_args'        => true,
+		'func_num_args'        => true,
+		'get_called_class'     => true,
+		'get_class'            => true,
+		'gettype'              => true,
+		'in_array'             => true,
+		'intval'               => true,
+		'is_array'             => true,
+		'is_bool'              => true,
+		'is_double'            => true,
+		'is_float'             => true,
+		'is_int'               => true,
+		'is_integer'           => true,
+		'is_long'              => true,
+		'is_null'              => true,
+		'is_object'            => true,
+		'is_real'              => true,
+		'is_resource'          => true,
+		'is_string'            => true,
+		'ord'                  => true,
+		'strlen'               => true,
+		'strval'               => true,
+		// @see https://github.com/php/php-src/blob/php-7.2.6/ext/opcache/Optimizer/pass1_5.c
+		'constant'             => true,
+		'define'               => true,
+		'dirname'              => true,
+		'extension_loaded'     => true,
+		'function_exists'      => true,
+		'is_callable'          => true,
+	];
 
 	/**
 	 * Returns an array of tokens this test wants to listen for.
@@ -21,7 +68,9 @@ final class FullyQualifiedGlobalFunctionsSniff implements Sniff {
 	 * @return array
 	 */
 	public function register(): array {
+
 		$this->globalFunctions = \array_flip( \get_defined_functions()['internal'] );
+
 		return [ \T_STRING ];
 	}
 
@@ -37,6 +86,14 @@ final class FullyQualifiedGlobalFunctionsSniff implements Sniff {
 	 * @return void
 	 */
 	public function process( File $phpcsFile, $stackPtr ) {
+
+		if ( $this->onlyOptimizedFunctions === null ) {
+			$this->onlyOptimizedFunctions = $this->get_ruleset_property( $phpcsFile, 'onlyOptimizedFunctions' );
+		}
+
+		if ( null !== $this->onlyOptimizedFunctions && false !== \filter_var( $this->onlyOptimizedFunctions, FILTER_VALIDATE_BOOLEAN ) ) {
+			$this->globalFunctions = $this->optimizedFunctions;
+		}
 
 		$this->stackPtr = $stackPtr;
 		$this->fixer    = $phpcsFile->fixer;
@@ -97,6 +154,29 @@ final class FullyQualifiedGlobalFunctionsSniff implements Sniff {
 	 */
 	private function fix( string $function ): void {
 		$this->fixer->replaceToken( $this->stackPtr, '\\' . $function );
+	}
+
+	/**
+	 * Get ruleset property
+	 *
+	 * - Check if the property is set on command line (--runtime-set <PROPERTY> <VALUE>), will override property in ruleset file.
+	 * - or, check if the property is set in the rulset file.
+	 *
+	 * @param File   $phpcsFile
+	 * @param string $property Property name.
+	 * @return mixed
+	 */
+	private function get_ruleset_property( File $phpcsFile, string $property ) {
+
+		print_r( $phpcsFile->ruleset->ruleset );
+
+		if ( null !== Config::getConfigData( $property ) ) {
+			return Config::getConfigData( $property );
+		} elseif ( isset( $phpcsFile->ruleset->ruleset[ $this->sniff ] ) === true && isset( $phpcsFile->ruleset->ruleset[ $this->sniff ]['properties'] ) === true && isset( $phpcsFile->ruleset->ruleset[ $this->sniff ]['properties'][ $property ] ) ) {
+			return $phpcsFile->ruleset->ruleset[ $this->sniff ]['properties'][ $property ];
+		} else {
+			return null;
+		}
 	}
 
 }
